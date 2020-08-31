7b116fcda2231eab2c02958a88641d1bb86c5870
comeme <developer.k.mshr0316@gmail.com>
2020-08-31 13:49:40 +0900

    initialize with skeleton

---
+++ myanalyzer_test.go
@@ -0,0 +1,14 @@
+package myanalyzer_test
+
+import (
+	"testing"
+
+	"github.com/komeme/myanalyzer"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+// TestAnalyzer is a test for Analyzer.
+func TestAnalyzer(t *testing.T) {
+	testdata := analysistest.TestData()
+	analysistest.Run(t, testdata, myanalyzer.Analyzer, "a")
+}
